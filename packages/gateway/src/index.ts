d797ae98b020972732782b5b04410b4f8923054f
Aki Wu <wuminzhe@gmail.com>
2025-03-07 21:36:50 +0800

    update

--- packages/gateway/src/index.ts
+++ packages/gateway/src/index.ts
@@ -11,7 +11,7 @@ program
     'Private key to sign responses with. Prefix with @ to read from a file'
   )
   .requiredOption('-d --darwinia-rpc-url <url>', 'Darwinia RPC URL')
-  .requiredOption('-c --darwinia-subname-registry-contract-address <address>', 'Darwinia Subname Registry Contract Address')
+  .requiredOption('-c --l2-resolver-contract-address <address>', 'L2 resolver contract address')
   .option('-t --ttl <number>', 'TTL for signatures', '300')
   .option('-p --port <number>', 'Port number to serve on', '8080');
 
@@ -28,13 +28,13 @@ if (privateKey.startsWith('@')) {
 const signingAddress = ethers.utils.computeAddress(privateKey);
 const signer = new ethers.utils.SigningKey(privateKey);
 
-const db = new DarwiniaDatabase(options.darwiniaRpcUrl, options.darwiniaSubnameRegistryContractAddress);
+const db = new DarwiniaDatabase(options.darwiniaRpcUrl, options.l2ResolverContractAddress);
 const app = makeApp(signer, '/', db);
 
 console.log(`Serving on port ${options.port}`);
 console.log(`signingAddress: ${signingAddress}`);
 console.log(`darwiniaRpcUrl: ${options.darwiniaRpcUrl}`);
-console.log(`darwiniaSubnameRegistry: ${options.darwiniaSubnameRegistryContractAddress}`);
+console.log(`l2ResolverContractAddress: ${options.l2ResolverContractAddress}`);
 
 app.listen(parseInt(options.port));
 
