d4554fcf9773ecada8f6814007806dcc278de815
Aki Wu <akiwu@Akis-Mac-mini-2.local>
2025-03-17 10:09:51 +0800

    update

--- packages/gateway/src/darwiniaDb.ts
+++ packages/gateway/src/darwiniaDb.ts
@@ -18,7 +18,7 @@ export class DarwiniaDatabase implements Database {
   }
 
   async addr(name: string, coinType: number): Promise<{ addr: string; ttl: number }> {
-    console.log('== querying address', name, coinType);
+    console.log(`   - querying address: "${name}", coinType: ${coinType}`);
 
     if (coinType !== 60) { // ETH coin type
       return { addr: ethers.constants.AddressZero, ttl: DEFAULT_TTL };
@@ -36,16 +36,16 @@ export class DarwiniaDatabase implements Database {
     try {
       const node = ethers.utils.namehash(name)
       const address = await this.contract.addr(node);
-      console.log('address of "', name, '" is', address);
+      console.log(`     address of "${name}" is "${address}"`);
       return { addr: address, ttl: DEFAULT_TTL };
     } catch (error) {
-      console.error('Error resolving name:', error);
+      console.error('     error resolving name:', error);
       return { addr: ethers.constants.AddressZero, ttl: DEFAULT_TTL };
     }
   }
 
   async text(name: string, key: string): Promise<{ value: string; ttl: number }> {
-    console.log('== querying text record', name, key);
+    console.log(`   - querying text record: "${name}", key: "${key}"`);
 
     if (name === 'darwinia.eth' || name.split('.').length > 3) {
       return { value: 'hello', ttl: DEFAULT_TTL };
@@ -54,16 +54,16 @@ export class DarwiniaDatabase implements Database {
     try {
       const node = ethers.utils.namehash(name)
       const record = await this.contract.text(node, key);
-      console.log('text record of "', name, '" is', record);
+      console.log(`     text record of "${name}" is "${record}"`);
       return { value: record, ttl: DEFAULT_TTL };
     } catch (error) {
-      console.error('Error resolving text record:', error);
+      console.error('     error resolving text record:', error);
       return { value: '', ttl: DEFAULT_TTL };
     }
   }
 
   async contenthash(name: string): Promise<{ contenthash: string; ttl: number }> {
-    console.log('== querying contenthash', name);
+    console.log(`   - querying contenthash: "${name}"`);
     // The SubnameRegistry doesn't support contenthash, so we'll return an empty string
     return { contenthash: '0x', ttl: DEFAULT_TTL };
   }
--- packages/gateway/src/server.ts
+++ packages/gateway/src/server.ts
@@ -109,7 +109,7 @@ export class Server extends OriginalServer {
   }
 
   async handleRequest(req: express.Request, res: express.Response) {
-    console.log('request path:', (req as any).path);
+    console.log('== request path:', (req as any).path);
     await super.handleRequest(req, res);
   }
 }
@@ -120,9 +120,9 @@ export function makeServer(signer: ethers.utils.SigningKey, db: Database) {
     {
       type: 'resolve',
       func: async ([encodedName, data]: Result, request) => {
-        console.log(`-- resolve("${encodedName}", "${data}") --`);
+        console.log(`   resolve("${encodedName}", "${data}")`);
         const name = decodeDnsName(Buffer.from(encodedName.slice(2), 'hex'));
-        console.log(`decoded name: ${name}`);
+        console.log(`   decoded name: "${name}"`);
         // Query the database
         const { result, validUntil } = await query(db, name, data);
 
